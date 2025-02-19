ed50600e7993625fe924b30a45bf813dd10733a7
alan_ <alan_@DESKTOP-NG54E7V>
2025-02-18 17:16:03 -0700

    Subir aqui

---
+++ src/main/java/twittermain/Twitterproyect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package twittermain;
+
+/**
+ *
+ * @author alan_
+ */
+public class Twitterproyect {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
