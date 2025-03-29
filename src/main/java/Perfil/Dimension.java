03ef396897c4d2458f6e7adf6ff16594249551d2
SrJaimee <jaimeandujo15@gmail.com>
2025-03-28 22:02:12 -0600

    Creacion de Interfaz EditPerfil

---
+++ src/main/java/Perfil/Dimension.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Perfil;
+
+/**
+ *
+ * @author Jaime Paredes
+ */
+public class Dimension extends java.awt.Dimension {
+
+    public Dimension(int i, int i0) {
+    }
+    
+}
